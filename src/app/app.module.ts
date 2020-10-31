647941ae72f6f8cf200eba59783c8b3f8b726a06
rezimOne <michalreda@gmail.com>
2020-10-31 08:20:13 +0100

    refactor(views module)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { ViewsModule } from './views/views.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -4,13 +4,14 @@ import { TodoComponent } from './todo/todo.component';
 import { StudentsComponent } from './students/students.component';
 import { NotesComponent } from './notes/notes.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [TodoComponent, StudentsComponent, NotesComponent, PageNotFoundComponent],
   exports: [TodoComponent, StudentsComponent, NotesComponent, PageNotFoundComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, SharedModule]
 })
 export class ViewsModule { }
